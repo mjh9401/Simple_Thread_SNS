4488d82ae23f17926ad5a994dae8d29bbccb03da
mjh9401@naver.com <mjh9401@naver.com>
2025-07-01 18:42:39 +0900

    로깅 추가

--- src/main/java/com/example/board/controller/PostController.java
+++ src/main/java/com/example/board/controller/PostController.java
@@ -2,6 +2,8 @@ package com.example.board.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,6 +23,8 @@ import com.example.board.service.PostService;
 @RestController
 @RequestMapping("/api/v1/posts")
 public class PostController {
+
+    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
 
     @Autowired
     private PostService postService;
